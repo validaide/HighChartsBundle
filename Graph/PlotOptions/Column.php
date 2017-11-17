81a22fa8e51a36d07ad4c53d02c31fe660e02dab
Mark Bijl <mark.bijl@validaide.com>
2017-11-17 13:08:16 +0100

    1550449-152746722 Remove the restriction on submitting Location Capabilities for review only at 100%, and allow the submittal when at least one answer is provided - introduce Column PlotOptions with stacking option [#152746722]

---
+++ Graph/PlotOptions/Column.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Validaide\HighChartsBundle\Graph\PlotOptions;
+
+use Validaide\HighChartsBundle\Graph\PlotOptions;
+
+/**
+ * Class Column
+ *
+ * @author Mark Bijl <mark.bijl@validaide.com>
+ */
+class Column extends PlotOptions
+{
+    const STACKING_NORMAL  = 'normal';
+    const STACKING_PERCENT = 'percent';
+
+    /**
+     * @var string
+     */
+    protected $stacking;
+
+    /**
+     * @return string|null
+     */
+    public function getStacking()
+    {
+        return $this->stacking;
+    }
+
+    /**
+     * @param string $stacking
+     */
+    public function setStacking(string $stacking)
+    {
+        $this->stacking = $stacking;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        $result = [];
+
+        if (!is_null($this->color)) {
+            $result['series']['color'] = $this->color;
+        }
+
+        if (!is_null($this->allowPointSelect)) {
+            $result['series']['allowPointSelect'] = $this->allowPointSelect;
+        }
+
+        if (!is_null($this->stacking)) {
+            $result['series']['stacking'] = $this->stacking;
+        }
+
+        return $result;
+    }
+}
