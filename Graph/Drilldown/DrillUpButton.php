5104885d7a13d64452755dff6aadaaacc3232f0c
Marcel Tuinstra <marceltuinstra@macbook-pro.home>
2020-08-25 11:21:10 +0200

    [TWEAK] - More options

--- Graph/Drilldown/DrillUpButton.php
+++ Graph/Drilldown/DrillUpButton.php
@@ -5,14 +5,22 @@ namespace Validaide\HighChartsBundle\Graph\Drilldown;
 class DrillUpButton
 {
     /** @var string */
+    private $align = 'right';
+    /** @var string */
     private $verticalAlign = 'top';
+    /** @var string|null */
+    private $x;
+    /** @var string|null */
+    private $y;
+    /** @var string */
+    private $relativeTo = 'spacingBox';
 
     /**
-     * @return string|null
+     * @param string $align
      */
-    public function getVerticalAlign(): ?string
+    public function setAlign(string $align): void
     {
-        return $this->verticalAlign;
+        $this->align = $align;
     }
 
     /**
@@ -23,15 +31,54 @@ class DrillUpButton
         $this->verticalAlign = $verticalAlign;
     }
 
+    /**
+     * @param string|null $x
+     */
+    public function setX(?string $x): void
+    {
+        $this->x = $x;
+    }
+
+    /**
+     * @param string|null $y
+     */
+    public function setY(?string $y): void
+    {
+        $this->y = $y;
+    }
+
+    /**
+     * @param string $relativeTo
+     */
+    public function setRelativeTo(string $relativeTo): void
+    {
+        $this->relativeTo = $relativeTo;
+    }
+
+
     public function toArray(): array
     {
         $result = [];
 
+        if (!is_null($this->align)) {
+            $result['position']['align'] = $this->align;
+        }
+
         if (!is_null($this->verticalAlign)) {
             $result['position']['verticalAlign'] = $this->verticalAlign;
         }
-        
-        $result['relativeTo'] = 'spacingBox';
+
+        if (!is_null($this->x)) {
+            $result['position']['x'] = $this->x;
+        }
+
+        if (!is_null($this->y)) {
+            $result['position']['y'] = $this->y;
+        }
+
+        if (!is_null($this->relativeTo)) {
+            $result['relativeTo'] = $this->relativeTo;
+        }
 
         return $result;
     }
