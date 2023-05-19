733d893f4fbab5b51dde0846ac970eca83aa99c9
validaide-mark-bijl <mark.bijl@validaide.com>
2023-05-19 17:20:28 +0200

    - update rector + phpstan dependencies - add more options to 'Pane' [#181060049]

--- Graph/Pane.php
+++ Graph/Pane.php
@@ -7,6 +7,8 @@ class Pane
     public ?int    $startAngle = null;
     public ?int    $endAngle   = null;
     public ?string $size       = null;
+    public ?string $background = null;
+    public ?array  $center     = null;
 
     /**
      * @return array<string, string>
@@ -15,15 +17,20 @@ class Pane
     {
         $result = [];
 
+        if (!is_null($this->startAngle)) {
+            $result['startAngle'] = $this->startAngle;
+        }
         if (!is_null($this->endAngle)) {
-            $result['endAngle'] = (string)$this->endAngle;
+            $result['endAngle'] = $this->endAngle;
         }
         if (!is_null($this->size)) {
             $result['size'] = (string)$this->size;
         }
-        if (!is_null($this->startAngle)) {
-            $result['startAngle'] = (string)$this->startAngle;
+        if (!is_null($this->center)) {
+            $result['center'] = $this->center;
         }
+
+        $result['background'] = $this->background;
 
         return $result;
     }
